7f86a352decd7409400b35ec46eb03b546c783d2
zunair-exe <zunairmemon520@gmail.com>
2026-01-18 16:30:27 +0500

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f42c37",
+        secondary: "#f42c37",
+        brandYellow: "#fdc62e",
+        brandGreen: "#2dcc6f",
+        brandBlue: "#1376f4",
+        brandWhite: "#eeeeee",
+      },
+      theme: {
+  extend: {
+    dropShadow: {
+      'custom': '-8px 4px 6px rgba(0, 0, 0, 0.4)',
+    }
+  }
+},
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
